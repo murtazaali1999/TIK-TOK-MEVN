8b9cc7bd7c4252bf696d79a17d0d4c00cef15ee3
Murtaza Ali <murtazaalimashadi1999@gmail.com>
2022-12-25 13:34:05 +0500

    forntend vuetify added backend port changed files added and deleted

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
-<template></template>
+<template>
+  <v-app>
+    <v-navbar app dark height="50" class="bg-red-darken-4"
+      >Hello Toolbar</v-navbar
+    >
+  </v-app>
+</template>
 
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
-<style scoped></style>
+<script setup lang="ts"></script>
---
+++ frontend/src/layouts/default/Default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script lang="ts">
+export default { setup() {}, components: {}, created() {} };
+</script>
+
+<style lang="scss"></style>
